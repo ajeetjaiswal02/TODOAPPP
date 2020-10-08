4435bd3df28b4848c628a475567b77a9c1ab7eaa
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2020-10-08 14:29:04 +0530

    made a list on the screen text bigger

---
+++ tempData.js
@@ -0,0 +1,61 @@
+// temporay data util firebase login
+export default tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Buy Eggs",
+        completed: true,
+      },
+      {
+        title: "Buy Groceries",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Leisure",
+    color: "#2912B9",
+    todos: [
+      {
+        title: "Buy Ps5",
+        completed: true,
+      },
+      {
+        title: "Buy Macbook",
+        completed: true,
+      },
+      {
+        title: "Buy Monitor 4k",
+        completed: true,
+      },
+    ],
+  },
+];
